5ff91f7487e8c68a77bef58e016b6590c9daf7cf
JakubChalupnik@gmail.com <JakubChalupnik@gmail.com@c3c75b79-482b-e469-17be-15265ae610ee>
2015-03-25 21:33:34 +0000

    Latest version commit, not sure what the changes were

--- trunk/MSP430/LedMatrixClock/main.c
+++ trunk/MSP430/LedMatrixClock/main.c
@@ -48,6 +48,7 @@
 
 static volatile word DcfPeriodLength;
 static volatile word DcfHighPulseLength;
+static volatile word DcfUpdated;
 
 //
 // External function declarations
@@ -56,6 +57,8 @@ static volatile word DcfHighPulseLength;
 void InitializeCpu (void);
 void DelayMs (word Ms);
 void InitLedMatrix (void);
+void DisplayCharacter (byte Character, byte Column, byte Row, const byte *Font);
+void SendBuffer (byte *Buffer, byte Offset);
 
 //
 // Local function prototypes
@@ -95,7 +98,7 @@ __interrupt void PORT1_ISR_HOOK(void) {
 void InitializeGpio (void) {
 
     P1OUT = 0;                          // Port 1 Output Register
-    P1DIR = DCF77_PON_BIT;              // Port 1 Direction Register
+    P1DIR = DCF77_PON_BIT | LED_REC_BIT; // Port 1 Direction Register
     P1IES = 0;                          // Port 1 Interrupt Edge Select Register
     P1IFG = 0;                          // Port 1 Interrupt Flag Register
 
@@ -151,13 +154,18 @@ void Port1InterruptHandler (void) {
 
   if (DCF77_PORT_IFG & DCF77_DATA_BIT) {        // Is this interrupt from DCF77?
     DCF77_PORT_IFG &= ~DCF77_DATA_BIT;          // Clear the pending interrupt
+	
+	P1OUT ^= LED_REC_BIT;
 
     if (Dcf77DataIn ()) {                       // DCF77 pin is high, that means rising edge
       DcfPeriodLength = TimerRead ();
       TimerStart ();
+      P1IES ^= DCF77_DATA_BIT;
       LedRedOn ();
+      DcfUpdated = 1;
     } else {                                    // DCF77 pin is low, that is falling edge
       DcfHighPulseLength = TimerRead ();
+      P1IES ^= DCF77_DATA_BIT;
       LedRedOff ();
     }
   }
@@ -186,8 +194,9 @@ void InitializeDcf77 (void) {
   // Pull PON high for a while, then pull it down to start the receiver.
   //
 
+  DelayMs (3000);
   Dcf77PonSet ();
-  DelayMs (500);
+  DelayMs (2000);
   Dcf77PonClear ();
 
   //
@@ -216,15 +225,121 @@ void InitializeDcf77 (void) {
 //      None.
 //
 
-int main (void) {
 
+extern const byte FontTable [];
+extern const byte FontTable3 [];
+extern byte ScreenBuffer [];
+
+
+int main (void) {
+	int Poisoned, PoisonedCounter;
+	int SecondCounter;
+//	dword FirstSample, SecondSample;
+	word CurrentBit;
+	word Minute, Hour, Day;
+	
   InitializeCpu ();
   InitializeGpio ();
-//  InitializeTimerA ();
-//  InitializeDcf77 ();
+  InitializeTimerA ();
+  InitializeDcf77 ();
   InitLedMatrix ();
 
-  while (1) {}
+  //
+  // Configure the DCF77 data in interrupt to rising edge, clear any pending interrupt and enable it
+  //
+  
+  P1IES &= ~DCF77_DATA_BIT;
+  P1IFG &= ~DCF77_DATA_BIT;
+  P1IE |= DCF77_DATA_BIT;
+   
+  DcfUpdated = 0;
+   
+  __enable_interrupt(); 
+  while (1) {
+  	
+  	if (DcfUpdated) {
+  		
+  		
+		if ((DcfPeriodLength > 0x3800) &&
+		    (DcfPeriodLength < 0x4000) 
+		    ) {												// The whole period is around 1sec, that's the correct DCF77 pulse
+			if ((DcfHighPulseLength > 0x300) && 
+				(DcfHighPulseLength < 0x680) 
+				) {											// Short pulse - logical 0
+				CurrentBit = 0;
+			} else if ((DcfHighPulseLength > 0x900) && 
+				(DcfHighPulseLength < 0xD00) 
+				) {											// Short pulse - logical 0
+				CurrentBit = 0x8000;
+			} else {
+				Poisoned = 1;
+			}
+			if (SecondCounter < 21) {	
+				// Do nothing, meteo and other info bits
+			} else if (SecondCounter < 28) {
+				// Minutes
+				Minute >>= 1;
+				Minute |= CurrentBit;
+			} else if (SecondCounter < 29) {
+				// Do nothing, parity for 21-28
+			} else if (SecondCounter < 35) {
+				// Hours
+				Hour >>= 1;
+				Hour |= CurrentBit;
+			} else if (SecondCounter < 36) {
+				// Parity for 29 - 35
+			} else if (SecondCounter < 42) {
+				// Day in month
+				Day >>= 1;
+				Day |= CurrentBit;
+			} else {
+				// Do nothing
+			}   						
+			SecondCounter++;
+		} else if ((DcfPeriodLength > 0x7000) && 
+				   (DcfPeriodLength < 0x9000)
+				   ) {
+			if (!Poisoned) {
+				// process the sample
+				Minute >>= 9;
+				Hour >>= 10;
+				Day >>= 10;
+				
+				DisplayCharacter ((Hour >> 4) + '0', 0, 0, FontTable);
+  				DisplayCharacter ((Hour & 0x0F) + '0', 8, 0, FontTable);
+  				DisplayCharacter ((Minute >> 4) + '0', 17, 0, FontTable);
+  				DisplayCharacter ((Minute & 0x0F) + '0', 25, 0, FontTable);				
+  				SendBuffer ((byte *) ScreenBuffer, 32);
+				
+			} else {
+				PoisonedCounter++;
+			}				   	
+			Poisoned = 0;
+			SecondCounter = 0;
+			Minute = Hour = Day = 0;
+		} else {
+			Poisoned = 1;
+		}  		
+  		
+  		
+  		
+  		
+  		
+  		
+  		
+  		
+  		
+  		
+  		
+  		
+  		
+  		
+  		
+  		
+  		
+  		
+  	}
+  }
 
   return 0;
 }
